9acb1ae5a0c1048a6015cc2edc10b25c42f1c520
eps <enrevol@gmail.com>
2018-06-18 15:41:35 +0700

    Forward cocos2d classes.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// Forward missing cocos2d classes and functions.
+import './Cocos2dFwd'
+
 export { NestedPrefab } from './NestedPrefab';
 export { Polygon } from './Polygon';
 export { StaticComponent } from './StaticComponent';
