9eebfbed7358b3ef85ab1f4f4a6679663a56f032
eps <enrevol@gmail.com>
2018-06-20 16:17:59 +0700

    Export HsvComponent.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { NestedPrefab } from './NestedPrefab';
 export { Polygon } from './Polygon';
 export { StaticComponent } from './StaticComponent';
+export { HsvComponent } from './HsvComponent';
