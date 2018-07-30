7827b2275eb25f03e251c2346e8447d5b7faeb13
eps <enrevol@gmail.com>
2018-07-30 09:31:10 +0700

    Fix polygon collider in UnselectableComponent.

--- src/utils/UnselectableComponent.ts
+++ src/utils/UnselectableComponent.ts
@@ -1,15 +1,19 @@
-const { ccclass, disallowMultiple, menu } = cc._decorator;
+const { ccclass, disallowMultiple, executeInEditMode, menu } = cc._decorator;
 
 @ccclass
 @disallowMultiple
+@executeInEditMode
 @menu('ee/UnselectableComponent')
 export class UnselectableComponent extends cc.Component {
     public onEnable(): void {
         if (CC_EDITOR) {
             let component = this.getComponent(cc.PolygonCollider);
             if (component !== null) {
-                let element = <SVGPolygonElement>(component.gizmo._root.dragArea.node);
-                element.style.pointerEvents = 'fill';
+                let gizmo = component.gizmo;
+                if (gizmo !== null) {
+                    let element = <SVGPolygonElement>(gizmo._root.dragArea.node);
+                    element.style.pointerEvents = 'none';
+                }
             }
         }
     };
@@ -18,8 +22,11 @@ export class UnselectableComponent extends cc.Component {
         if (CC_EDITOR) {
             let component = this.getComponent(cc.PolygonCollider);
             if (component !== null) {
-                let element = <SVGPolygonElement>(component.gizmo._root.dragArea.node);
-                element.style.pointerEvents = 'none';
+                let gizmo = component.gizmo;
+                if (gizmo !== null) {
+                    let element = <SVGPolygonElement>(gizmo._root.dragArea.node);
+                    element.style.pointerEvents = 'fill';
+                }
             }
         }
     };
